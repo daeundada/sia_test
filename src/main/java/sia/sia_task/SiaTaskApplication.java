336112bf1eacf37f13acaa6d07732b3d10ca26b4
de123456sdf <cdaeun95@gmail.com>
2025-04-26 17:26:36 +0900

    git init

---
+++ src/main/java/sia/sia_task/SiaTaskApplication.java
@@ -0,0 +1,13 @@
+package sia.sia_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiaTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiaTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sia/sia_task/SiaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package sia.sia_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiaTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
